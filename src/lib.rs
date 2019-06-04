a2708c35418197fee01a5f76addfdec80c993ae7
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2019-06-04 11:29:12 +0100

    style/all: fix wrong copyright block and various style/clippy fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,60 @@
 // Copyright 2019 MaidSafe.net limited.
 //
-// This SAFE Network Software is licensed to you under the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT> or the Modified
-// BSD license <LICENSE-BSD or https://opensource.org/licenses/BSD-3-Clause>,
-// at your option. This file may not be copied, modified, or distributed
-// except according to those terms. Please review the Licences for the
-// specific language governing permissions and limitations relating to use
-// of the SAFE Network Software.
+// This SAFE Network Software is licensed to you under the MIT license <LICENSE-MIT
+// https://opensource.org/licenses/MIT> or the Modified BSD license <LICENSE-BSD
+// https://opensource.org/licenses/BSD-3-Clause>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms. Please review the Licences for the
+// specific language governing permissions and limitations relating to use of the SAFE Network
+// Software.
 
-#[macro_use]
-extern crate serde_derive;
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/maidsafe/QA/master/Images/maidsafe_logo.png",
+    html_favicon_url = "https://maidsafe.net/img/favicon.ico",
+    test(attr(forbid(warnings)))
+)]
+#![forbid(
+    exceeding_bitshifts,
+    mutable_transmutes,
+    no_mangle_const_items,
+    unknown_crate_types,
+    warnings
+)]
+#![deny(
+    bad_style,
+    deprecated,
+    improper_ctypes,
+    missing_docs,
+    non_shorthand_field_patterns,
+    overflowing_literals,
+    plugin_as_library,
+    stable_features,
+    unconditional_recursion,
+    unknown_lints,
+    unsafe_code,
+    unused,
+    unused_allocation,
+    unused_attributes,
+    unused_comparisons,
+    unused_features,
+    unused_parens,
+    while_true
+)]
+#![warn(
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results
+)]
+#![allow(
+    box_pointers,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    variant_size_differences
+)]
+// FIXME - write docs
+#![allow(missing_docs)]
 
 pub mod errors;
 pub mod immutable_data;
@@ -19,13 +64,14 @@ pub mod response;
 
 pub use immutable_data::{ImmutableData, UnpubImmutableData, MAX_IMMUTABLE_DATA_SIZE_IN_BYTES};
 use routing::MessageId as OldMessageId;
+use serde::{Deserialize, Serialize};
 
 /// Constant byte length of `XorName`.
 pub const XOR_NAME_LEN: usize = 32;
 
 /// A [`XOR_NAME_BITS`](constant.XOR_NAME_BITS.html)-bit number, viewed as a point in XOR space.
 ///
-/// This wraps an array of [`XOR_NAME_LEN`](constant.XOR_NAME_LEN.html) bytes, i. e. a number
+/// This wraps an array of [`XOR_NAME_LEN`](constant.XOR_NAME_LEN.html) bytes, i.e. a number
 /// between 0 and 2<sup>`XOR_NAME_BITS`</sup> - 1.
 ///
 /// XOR space is the space of these numbers, with the [XOR metric][1] as a notion of distance,
--- src/request.rs
+++ src/request.rs
@@ -1,18 +1,18 @@
 // Copyright 2019 MaidSafe.net limited.
 //
-// This SAFE Network Software is licensed to you under the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT> or the Modified
-// BSD license <LICENSE-BSD or https://opensource.org/licenses/BSD-3-Clause>,
-// at your option. This file may not be copied, modified, or distributed
-// except according to those terms. Please review the Licences for the
-// specific language governing permissions and limitations relating to use
-// of the SAFE Network Software.
+// This SAFE Network Software is licensed to you under the MIT license <LICENSE-MIT
+// https://opensource.org/licenses/MIT> or the Modified BSD license <LICENSE-BSD
+// https://opensource.org/licenses/BSD-3-Clause>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms. Please review the Licences for the
+// specific language governing permissions and limitations relating to use of the SAFE Network
+// Software.
 
 use crate::immutable_data::UnpubImmutableData;
 use crate::mutable_data::{MutableDataRef, SeqMutableData, UnseqMutableData};
 use crate::MessageId;
 use crate::XorName;
 use rust_sodium::crypto::sign;
+use serde::{Deserialize, Serialize};
 
 /// RPC Request that is sent to vaults
 #[allow(clippy::large_enum_variant)]
