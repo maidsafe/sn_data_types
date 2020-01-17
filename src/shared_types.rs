fd671f6f1cd7259124ec6f265e06ff51c0d4938c
oetyng <oetyng@gmail.com>
2020-01-17 18:57:29 +0100

    docs/shared_types: add missing docs

--- src/shared_types.rs
+++ src/shared_types.rs
@@ -13,10 +13,12 @@ use serde::{Deserialize, Serialize};
 use std::ops::Deref;
 use std::ops::Range;
 
+/// A Key used to relate to a value.
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, Default, Debug)]
 pub struct Key(Vec<u8>);
 
 impl Key {
+    /// Returns the underlying implementation, a byte array.
     pub fn get(&self) -> Vec<u8> {
         self.0.to_vec()
     }
@@ -36,9 +38,11 @@ impl From<Vec<u8>> for Key {
     }
 }
 
+/// A value stored in a data type.
 pub type Value = Vec<u8>;
-pub type KeyValuePair = (Key, Value);
+/// A list of values.
 pub type Values = Vec<Value>;
+/// A list of keys.
 pub type Keys = Vec<Key>;
 
 /// Marker for Guarded data.
@@ -49,19 +53,25 @@ pub struct Guarded;
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, Debug)]
 pub struct NonGuarded;
 
+/// Represents users of the network.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, Debug)]
 pub enum User {
+    /// The category representing any user.
     Anyone,
+    /// The category representing a specific user.
     Specific(PublicKey),
 }
 
 /// The current version is defined as the last entry in the vector.
 pub const CURRENT_VERSION: Version = Version::FromEnd(1); // Shouldn't last entry be accessed with 0 ?
 
+///
 #[derive(Copy, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
 pub enum Version {
-    FromStart(u64), // Absolute index
-    FromEnd(u64),   // Relative Version - start counting from the end
+    /// Absolute index
+    FromStart(u64),
+    /// Relative Version - start counting from the end
+    FromEnd(u64),
 }
 
 impl From<u64> for Version {
@@ -70,7 +80,7 @@ impl From<u64> for Version {
     }
 }
 
-// Set of data, owners, permissions versions.
+/// Set of data, owners, permissions versions.
 #[derive(Copy, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
 pub struct ExpectedVersions {
     expected_data_version: u64,
@@ -79,6 +89,7 @@ pub struct ExpectedVersions {
 }
 
 impl ExpectedVersions {
+    /// ctor
     pub fn new(
         expected_data_version: u64,
         expected_owners_version: u64,
@@ -91,21 +102,27 @@ impl ExpectedVersions {
         }
     }
 
+    /// Returns the expected version for the data.
     pub fn expected_data_version(&self) -> u64 {
         self.expected_data_version
     }
 
+    /// Returns the expected version for the owner.
     pub fn expected_owners_version(&self) -> u64 {
         self.expected_owners_version
     }
 
+    /// Returns the expected version for the access list.
     pub fn expected_access_list_version(&self) -> u64 {
         self.expected_access_list_version
     }
 }
 
+/// Represents an owner of a data instance,
+/// at a specific data and access list version.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, Debug)]
 pub struct Owner {
+    /// The public key of the user being the owner.
     pub public_key: PublicKey,
     /// The expected Version of the data at the time this ownership change is to become valid.
     pub expected_data_version: u64,
@@ -113,23 +130,32 @@ pub struct Owner {
     pub expected_access_list_version: u64,
 }
 
+/// The flavour of the data type.
 #[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize, Debug)]
 pub enum Kind {
+    /// Public and with concurrency control.
     PublicGuarded,
+    /// Public means data is perpetual.
     Public,
+    /// Private and with concurrency control.
     PrivateGuarded,
+    /// Private means data can be deleted, if desired.
     Private,
 }
 
 impl Kind {
+    /// Returns true if the data type is public.
     pub fn is_public(self) -> bool {
         self == Kind::PublicGuarded || self == Kind::Public
     }
 
+    /// Returns true if the data type is private.
     pub fn is_private(self) -> bool {
         !self.is_public()
     }
 
+    /// Returns true if the data type
+    /// is guarded, i.e. implements concurrency control.
     pub fn is_guarded(self) -> bool {
         self == Kind::PublicGuarded || self == Kind::PrivateGuarded
     }
@@ -151,15 +177,43 @@ pub enum DataAddress {
     Sequence(Address),
 }
 
+/// The address of a data type.
+/// Each flavour has its own address space.
+/// Todo: Fix this formatting / docs.
 #[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize, Debug)]
 pub enum Address {
-    PublicGuarded { name: XorName, tag: u64 },
-    Public { name: XorName, tag: u64 },
-    PrivateGuarded { name: XorName, tag: u64 },
-    Private { name: XorName, tag: u64 },
+    ///
+    PublicGuarded {
+        ///
+        name: XorName,
+        ///
+        tag: u64,
+    },
+    ///
+    Public {
+        ///
+        name: XorName,
+        ///
+        tag: u64,
+    },
+    ///
+    PrivateGuarded {
+        ///
+        name: XorName,
+        ///
+        tag: u64,
+    },
+    ///
+    Private {
+        ///
+        name: XorName,
+        ///
+        tag: u64,
+    },
 }
 
 impl Address {
+    /// Returns an Address instance for the specified flavour.
     pub fn from_kind(kind: Kind, name: XorName, tag: u64) -> Self {
         match kind {
             Kind::PublicGuarded => Address::PublicGuarded { name, tag },
@@ -169,6 +223,7 @@ impl Address {
         }
     }
 
+    /// Returns the flavour that this address space represents.
     pub fn kind(&self) -> Kind {
         match self {
             Address::PublicGuarded { .. } => Kind::PublicGuarded,
@@ -178,6 +233,8 @@ impl Address {
         }
     }
 
+    /// Returns the XorName that the data is located by
+    /// within its address space.
     pub fn name(&self) -> &XorName {
         match self {
             Address::PublicGuarded { ref name, .. }
@@ -187,6 +244,7 @@ impl Address {
         }
     }
 
+    /// Returns the tag that denotes a specific address space.
     pub fn tag(&self) -> u64 {
         match self {
             Address::PublicGuarded { tag, .. }
@@ -196,14 +254,18 @@ impl Address {
         }
     }
 
+    /// Returns true if the address is for a public data type.
     pub fn is_public(&self) -> bool {
         self.kind().is_public()
     }
 
+    /// Returns true if the address is for a private data type.
     pub fn is_private(&self) -> bool {
         self.kind().is_private()
     }
 
+    /// Returns true if the address is for a guarded data type,
+    /// i.e. a data type implementing concurrency control.
     pub fn is_guarded(&self) -> bool {
         self.kind().is_guarded()
     }
@@ -213,7 +275,7 @@ impl Address {
         utils::encode(&self)
     }
 
-    /// Create from z-base-32 encoded string.
+    /// Creates from z-base-32 encoded string.
     pub fn decode_from_zbase32<I: Decodable>(encoded: I) -> Result<Self> {
         utils::decode(encoded)
     }
